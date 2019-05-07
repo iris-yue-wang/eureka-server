f310d328e70b6408a0fe460f62e56fbc4bf49540
Wang, Yue <Yue.Wang@zuhlke.com>
2019-05-07 15:03:31 +0100

    initial commit

---
+++ src/main/java/com/zuhlke/eureka/server/Application.java
@@ -0,0 +1,15 @@
+package com.zuhlke.eureka.server;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(WebApplicationType.SERVLET).run(args);
+    }
+}
